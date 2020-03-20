4555d1e9ca93590aa237dbde72f657ff0438793b
zeinsalahppu <zeinsalah@gmail.com>
2020-03-20 20:59:22 +0200

    written class as module

---
+++ LibClass.py
@@ -0,0 +1,61 @@
+# print(__name__)
+
+class LibraryItem:
+    def __init__(self, **kwargs):
+        self.id = kwargs.pop("id")
+        self.title = kwargs.pop("title")
+
+    def print_info(self):
+        print("-------- Library Item Details --------")
+        print("Item ID: " + str(self.id))
+        print("Title: " + self.title)
+
+
+class Book(LibraryItem):
+    def __init__(self, **kwargs):
+        self.author = kwargs.pop("author")
+        self.isbn = kwargs.pop("isbn")
+        super().__init__(**kwargs)
+
+    def print_info(self):
+        super().print_info()
+        print("-------- Book Details --------")
+        print("Author: " + self.author)
+        print("ISBN: " + str(self.isbn))
+
+    def read(self):
+        print("I am reading the book \"" + self.title + "\" for you")
+
+
+class SoundTrack(LibraryItem):
+    def __init__(self, **kwargs):
+        self.singer = kwargs.pop("singer")
+        self.year = kwargs.pop("year")
+        super().__init__(**kwargs)
+
+    def print_info(self):
+        super().print_info()
+        print("-------- Sound Track Details --------")
+        print("Artist: " + self.singer)
+        print("Production Year: " + str(self.year))
+
+    def play(self):
+        print("I am playing track \"" + self.title + "\" for you")
+
+
+class AudioBook(Book, SoundTrack):
+    def __init__(self, id, title,  author, isbn, singer, year):
+        super().__init__(id=id, title=title, author=author, isbn=isbn, singer=singer, year=year)
+
+    def print_info(self):
+        super().print_info()
+
+
+def details(obj):
+    obj.print_info()
+
+
+if __name__ == "__main__":
+    ab1 = AudioBook(54, "Happy Farmer", "Sandy", "333-47538745", "Sally", 2010)
+    print(AudioBook.mro())
+
--- MultipleInheritance.py
+++ MultipleInheritance.py
@@ -1,71 +1,16 @@
+import LibClass
 
-class LibraryItem:
-    def __init__(self, **kwargs):
-        print("Initializing LibraryItem")
-        self.id = kwargs.pop("id")
-        self.title = kwargs.pop("title")
+print(LibClass)
 
-    def print_info(self):
-        print("-------- Library Item Details --------")
-        print("Item ID: " + str(self.id))
-        print("Title: " + self.title)
+my_libraryitem = LibClass.LibraryItem(id=77, title="Python for Beginners")
 
+my_book = LibClass.Book(id=104, title="Data Visualization", author="Muller", isbn="123-1234567890")
+my_book.read()
 
-# item1 = LibraryItem(id=77, title="Python for Beginners")
-# item1.print_info()
+my_soundtrack = LibClass.SoundTrack(id=9, title="Winds of the West", singer="Westermann", year=2007)
+my_soundtrack.play()
 
+my_audiobook = LibClass.AudioBook(54, "Happy Farmer", "Sandy", "333-47538745", "Sally", 2010)
+LibClass.details(my_audiobook)
 
-class Book(LibraryItem):
-    def __init__(self, **kwargs):
-        print("Initializing Book")
-        self.author = kwargs.pop("author")
-        self.isbn = kwargs.pop("isbn")
-        super().__init__(**kwargs)
-
-    def print_info(self):
-        super().print_info()
-        print("-------- Book Details --------")
-        print("Author: " + self.author)
-        print("ISBN: " + str(self.isbn))
-
-    def read(self):
-        print("I am reading the book \"" + self.title + "\" for you")
-
-# b1 = Book(id=104, title="Data Visualization", author="Muller", isbn="123-1234567890")
-# b1.print_info()
-# b1.read()
-
-
-class SoundTrack(LibraryItem):
-    def __init__(self, **kwargs):
-        print("Initializing SoundTrack")
-        self.singer = kwargs.pop("singer")
-        self.year = kwargs.pop("year")
-        super().__init__(**kwargs)
-
-    def print_info(self):
-        super().print_info()
-        print("-------- Sound Track Details --------")
-        print("Artist: " + self.singer)
-        print("Production Year: " + str(self.year))
-
-    def play(self):
-        print("I am playing track \"" + self.title + "\" for you")
-
-# f1 = SoundTrack(id=9, title="Winds of the West", singer="Westermann", year=2007)
-# f1.print_info()
-# f1.play()
-
-
-class AudioBook(Book, SoundTrack):
-    def __init__(self, id, title,  author, isbn, singer, year):
-        print("Initializing AudioBook")
-        super().__init__(id=id, title=title, author=author, isbn=isbn, singer=singer, year=year)
-
-    def print_info(self):
-        super().print_info()
-
-
-ab1 = AudioBook(54, "Happy Farmer", "Sandy", "333-47538745", "Sally", 2010)
-print(AudioBook.mro())
-
+# print(__name__)
